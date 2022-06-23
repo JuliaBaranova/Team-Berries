e820cc244fec764d5f4b34171c811baa3e37895e
Nadezhda Bukavnyova <bukavneva99@mail.ru>
2022-06-23 10:41:30 +0300

    feature/search

--- src/products.js
+++ src/products.js
@@ -1,5 +1,6 @@
 import { getProducts, putProducts } from "./localstorage";
 const list = document.querySelector('ul')
+const search = document.querySelector('.header__input')
 
 export function renderCatalog(catalog) {
     const productsStore = getProducts();
@@ -12,29 +13,44 @@ export function renderCatalog(catalog) {
         const itemPrice = itemContainer.querySelector('.products-item__price')
         const itemName = itemContainer.querySelector('.products-item__name')
         const itemBtn = itemContainer.querySelector('.products-item__btn')
-       
+
         itemContainer.classList.add(element.selector)
         itemImg.src = element.image
         itemPrice.innerText = element.price + ' BYN'
         itemName.innerText = element.name
-        
-        list.append(li)    
-        
-itemBtn.addEventListener('click', function () {
-    const { pushProduct, products } = putProducts(id);
-    if (pushProduct) {
-        itemBtn.innerText = 'Из корзины';
-    } else {
-        itemBtn.innerText = 'В корзину';
-    }
-})
- if (productsStore.indexOf(id) === -1) {
+
+        list.append(li)
+
+        itemBtn.addEventListener('click', function () {
+            const { pushProduct, products } = putProducts(id);
+            if (pushProduct) {
+                itemBtn.innerText = 'Из корзины';
+            } else {
+                itemBtn.innerText = 'В корзину';
+            }
+        })
+        if (productsStore.indexOf(id) === -1) {
             itemBtn.textContent = 'В корзину';
         } else {
             itemBtn.textContent = 'Из корзины';
         }
 
-})  
-}  
+    })
+}
 
+search.addEventListener('input', () => {
+    const searchValue = search.value.toLowerCase()
+    Array.from(list.children).forEach((node) => {
+        const itemContainer = node.querySelector('.products-item__name')
+        const itemName = itemContainer.textContent.toLowerCase()
+        if (!itemName.includes(searchValue)) {
+            if (!node.classList.contains('hidden')) {
+                node.classList.add('hidden')
+            }
+        } else if (node.classList.contains('hidden')) {
+            node.classList.remove('hidden')
+        }
+    })
+    renderCatalog()
+})
 
